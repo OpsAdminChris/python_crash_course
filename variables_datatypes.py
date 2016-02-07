e363add21d5c110acbd902f9f9d509868c692697
Christopher Roberts <chris@opsadmin.net>
2016-02-07 13:15:26 -0500

    basic strings and methods

---
+++ variables_datatypes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+message = "Hello World!"
+print(message)
+
+string = 'This is a new string'
+print(string)
+
+string = 'replacing the original text'
+print(string)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + ' ' + last_name
+print("Hello " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
