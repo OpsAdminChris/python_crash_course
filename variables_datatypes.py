504167fb244f0e13f089fe26ece43b42c5625182
Christopher Roberts <chris@opsadmin.net>
2016-02-13 11:32:14 -0500

    modified with 'Migration2to3' plugin

--- variables_datatypes.py
+++ variables_datatypes.py
@@ -9,14 +9,14 @@ string = 'replacing the original text'
 print(string)
 
 name = "ada lovelace"
-print(name.title())
-print(name.upper())
-print(name.lower())
+print((name.title()))
+print((name.upper()))
+print((name.lower()))
 
 first_name = 'ada'
 last_name = 'lovelace'
 full_name = first_name + ' ' + last_name
-print("Hello " + full_name.title() + "!")
+print(("Hello " + full_name.title() + "!"))
 
 message = "Hello, " + full_name.title() + "!"
 print(message)
